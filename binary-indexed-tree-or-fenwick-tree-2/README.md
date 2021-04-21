a7f7631d18625e95ba8b13cbb1a5f3ee8127d820
graibn <5966878+t1ooo@users.noreply.github.com>
2021-04-21 14:19:47 +0500

    update

---
+++ binary-indexed-tree-or-fenwick-tree-2/README.md
@@ -0,0 +1,1 @@
+https://www.geeksforgeeks.org/binary-indexed-tree-or-fenwick-tree-2/
---
+++ binary-indexed-tree-or-fenwick-tree-2/main.py
@@ -0,0 +1,35 @@
+def getsum(bittree, i):
+    res = 0
+    i = i+1
+    while i > 0:
+        res += bittree[i]
+        i -= i & (-i)
+        # i = i & (i-1)
+
+    return res
+
+
+def updatebits(bittree, n, i, v):
+    i += 1
+    while i <= n:
+        bittree[i] += v
+        i += i & (-i)
+        # i += i & (i-1)
+
+
+def construct(arr, n):
+    bittree = [0] * (n+1)
+    for i in range(n):
+        updatebits(bittree, n, i, arr[i])
+
+    return bittree
+
+
+freq = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
+
+bittree = construct(freq, len(freq))
+assert 12 == getsum(bittree, 5)
+
+freq[3] += 6
+updatebits(bittree, len(freq), 3, 6)
+assert 18 == getsum(bittree, 5)
