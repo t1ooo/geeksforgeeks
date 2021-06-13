2711103ecc40e42e521dc7d618513b41983930eb
graibn <5966878+t1ooo@users.noreply.github.com>
2021-06-13 14:19:31 +0500

    update

---
+++ union-find-algorithm-set-2-union-by-rank/main.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, num_of_v):
+        self.num_of_v = num_of_v
+        self.edges = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.edges[u].append(v)
+
+
+class Subset:
+    def __init__(self, parent, rank):
+        self.parent = parent
+        self.rank = rank
+
+
+def find(subsets, node):
+    if subsets[node].parent != node: # is node not parent to itself?
+        subsets[node].parent = find(subsets, subsets[node].parent)
+    return subsets[node].parent
+
+
+def union(subsets, u, v):
+    if subsets[u].rank > subsets[v].rank:
+        subsets[v].parent = u
+    elif subsets[v].rank > subsets[u].rank:
+        subsets[u].parent = v
+    else:
+        subsets[v].parent = u
+        subsets[u].rank += 1
+
+
+def isCycle(graph):
+    subsets = []
+
+    for u in range(graph.num_of_v):
+        subsets.append(Subset(u, 0))
+
+    for u in graph.edges:
+        u_rep = find(subsets, u)
+
+        for v in graph.edges[u]:
+            v_rep = find(subsets, v)
+
+            if u_rep == v_rep:
+                return True
+            else:
+                union(subsets, u_rep, v_rep)
+
+
+g = Graph(3)
+
+g.add_edge(0, 1)
+g.add_edge(1, 2)
+g.add_edge(0, 2)
+
+assert isCycle(g) == True
